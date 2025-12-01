b6d8ba0e30ae2d4cb39aca4f336ece05c5ce26f7
PurvaJadhav <purvajadhav1604@gmail.com>
2025-12-02 01:05:01 +0530

    add redux toolkit project

---
+++ reduxToolkitProject/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className='text-2xl font-bold text-white'> Todo with Redux Tolkit</div>
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
