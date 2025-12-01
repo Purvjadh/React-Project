4bff05347658aed3923d5c15fe89c8ac8ddd562f
PurvaJadhav <purvajadhav1604@gmail.com>
2025-12-01 13:26:40 +0530

    add practice Todo App Project with Context Api

---
+++ todoContextPractice/src/contexts/TodoContext.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { createContext,useContext } from "react";
+
+export const TodoContext=createContext({
+    todos:[
+        {
+        id:1,
+        todo:'todo Msg',
+        completed:false,
+        }
+    ],
+    addTodo: (todo) => {},
+
+    updateTodo:(id,todo) => {},
+
+    deleteTodo:(id) => {},
+
+    toggleComplete:() => {}
+})
+
+export const TodoContextProvider=TodoContext.Provider
+
+export const useTodo = () => {
+   return useContext(TodoContext)
+}
