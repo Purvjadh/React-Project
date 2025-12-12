06d0af08c05e7bd9c48a997d6c0ff22f29fc977c
PurvaJadhav <purvajadhav1604@gmail.com>
2025-12-12 12:06:05 +0530

    add redux toolkit setup and some changes in older files

--- 14megaBlogProject/src/App.jsx
+++ 14megaBlogProject/src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css'
+import React , {useState,useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header,Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  const [loading,setLoading] = useState(true)
 
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
-  )
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .catch((error) => console.log(error))
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap bg-slate-500 content-between'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : 
+  null
 }
+
+
 
 export default App
--- 14megaBlogProject/src/appwrite/auth.js
+++ 14megaBlogProject/src/appwrite/auth.js
@@ -39,7 +39,7 @@ export class AuthService {
         try {
             return await this.account.get()
         } catch (error) {
-            console.log('Appwrite service :: getCurrentUser error',error)
+            console.log('appwrite service :: getCurrentUser ::error',error)
         }
 
         return null
